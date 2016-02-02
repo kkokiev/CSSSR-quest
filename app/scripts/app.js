517b3fa52135105a7baa2f6b7b11d4b78139c1a7
Kaeserkiev <kaeserkiev@gmail.com>
2016-02-02 12:10:31 +0200

    quest markup done

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,45 @@
-import svg4everybody from 'svg4everybody';
-import $ from 'jquery';
+var sliderElem = document.getElementById('slider');
+var thumbElem = sliderElem.children[0];
 
-$(() => {
-	svg4everybody();
-});
+	thumbElem.onmousedown = function(e) {
+		var thumbCoords = getCoords(thumbElem);
+		var shiftX = e.pageX - thumbCoords.left;
+
+
+		var sliderCoords = getCoords(sliderElem);
+
+		document.onmousemove = function(e) {
+			var newLeft = e.pageX - shiftX - sliderCoords.left;
+
+			if (newLeft < 0) {
+				newLeft = 0;
+			}
+
+			var rightEdge = sliderElem.offsetWidth - thumbElem.offsetWidth;
+			if (newLeft > rightEdge) {
+				newLeft = rightEdge;
+			}
+
+			thumbElem.style.left = newLeft + 'px';
+		}
+
+		document.onmouseup = function() {
+			document.onmousemove = document.onmouseup = null;
+		};
+
+		return false;
+	};
+
+		thumbElem.ondragstart = function() {
+			return false;
+		};
+
+		function getCoords(elem) {
+			var box = elem.getBoundingClientRect();
+
+			return {
+				top: box.top + pageYOffset,
+				left: box.left + pageXOffset
+			};
+
+		}
